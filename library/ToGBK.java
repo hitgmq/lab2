6cc5fabd95be69a54be3d1dfe7d334914491d9d7
gmq <1352288243@qq.com>
2014-10-17 20:07:03 +0800

    hd

---
+++ library/ToGBK.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package library;
+c1
+changge 2
+/**
+ *
+ * @author xia
+ */
+public class ToGBK{
+
+    public static String toGBK(String s){
+        String str = s;
+        try{
+            if(s == null){
+                return null;
+            }
+            else{
+                str = new String(s.getBytes("ISO-8859-1"), "GBK");
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+
+        return str;
+    }
+}
